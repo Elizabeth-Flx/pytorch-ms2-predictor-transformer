33396825f61e909d5bde4812e5b5c4e06948303e
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2025-01-28 21:56:41 +0100

    implemented most of the init in TransMS2Predictor

--- model/model.py
+++ model/model.py
@@ -2,6 +2,8 @@
 import torch
 import torch.nn as nn
 
+import model_parts as mp
+
 
 class TransMS2Predictor(nn.Module):
     
@@ -10,16 +12,27 @@ class TransMS2Predictor(nn.Module):
 
         self.embedding_dim = 256
         self.n_transblocks = 8
+        self.penult_dim = 512
+
+        self.output_units = 174
 
         # Embedding (onehot to embedding)
         self.peptide_embedder = nn.Linear(self.embedding_dim)
 
         self.trans_blocks = [
-            
-
-
-            for _ in range(n_transblocks)
+            # todo: add variables
+            mp.TransBlock()
+            for _ in range(self.n_transblocks)
         ]
+
+        self.penult_linear = nn.Linear(self.penult_dim)
+        self.penult_norm = nn.LayerNorm(self.penult_dim)
+        self.relu = nn.ReLU()
+        self.final_linear = nn.Linear(self.output_units)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        pass
 
 
 
