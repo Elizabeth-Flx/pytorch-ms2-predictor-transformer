ed287ce45fbcaa6e3b057d97f500c6907b79d4fa
Elizabeth-Flx <daniel.lochert.flx@gmail.com>
2025-01-29 19:57:28 +0100

    added model_testing notebook

--- model/model.py
+++ model/model.py
@@ -4,6 +4,31 @@ import torch.nn as nn
 
 import model_parts as mp
 
+CUSTOM_ALPHABET = {
+    'A': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'K': 9,
+    'L': 10,
+    'M': 11,
+    'N': 12,
+    'P': 13,
+    'Q': 14,
+    'R': 15,
+    'S': 16,
+    'T': 17,
+    'V': 18,
+    'W': 19,
+    'Y': 20,
+    'm': 21, # oxidized methionine
+}
+ALPHABET_SIZE = max(CUSTOM_ALPHABET.values())
+
 
 class TransMS2Predictor(nn.Module):
     
@@ -17,25 +42,41 @@ class TransMS2Predictor(nn.Module):
         self.output_units = 174
 
         # Embedding (onehot to embedding)
-        self.peptide_embedder = nn.Linear(self.embedding_dim)
+        self.peptide_embedder = nn.Linear(ALPHABET_SIZE, self.embedding_dim)
+
+        # Positional Encoding
+        self.pos_enc = mp.PositionalEncoding(self.embedding_dim)
 
         self.trans_blocks = [
             # todo: add variables
-            mp.TransBlock()
+            mp.TransBlock(self.embedding_dim, 8, 128)
             for _ in range(self.n_transblocks)
         ]
 
-        self.penult_linear = nn.Linear(self.penult_dim)
+        self.penult_linear = nn.Linear(self.embedding_dim, self.penult_dim)
         self.penult_norm = nn.LayerNorm(self.penult_dim)
         self.relu = nn.ReLU()
-        self.final_linear = nn.Linear(self.output_units)
+        self.final_linear = nn.Linear(self.penult_dim, self.output_units)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+        
+
+
+
         pass
 
 
 
 
+test = "/cmnfs/data/proteomics/Prosit_PTMs/Transformer_Train/no_aug_test.parquet"
+
+
+
+
+import pandas as pd
+tmp = pd.read_parquet(test, engine='pyarrow')
+
+print(tmp)
 
 
